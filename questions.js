3b435159d3ea38b46a7b7111d2a2be4b17cbe7eb
Codeinx <aseyeged@gmail.com>
2024-08-27 03:09:56 +0100

    Working now

---
+++ questions.js
@@ -0,0 +1,563 @@
+let quest = [
+    {
+        numb: 1,
+        question:"What is the Primary function of Javascript?",
+        answer: "Creating dynamic behaviours",
+        options: [
+            "Creating dynamic behaviours",
+            "Styling web pages",
+            "Sending emails",
+            "Managing databases"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the primary purpose of HTML?",
+        answer: "Structuring web content",
+        options: [
+            "Structuring web content",
+            "Styling web content",
+            "Adding interactivity",
+            "Managing server databases"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the following is a JavaScript framework?",
+        answer: "React",
+        options: [
+            "Django",
+            "Laravel",
+            "React",
+            "Ruby on Rails"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is CSS primarily used for?",
+        answer: "Styling web pages",
+        options: [
+            "Structuring web pages",
+            "Styling web pages",
+            "Creating databases",
+            "Programming server logic"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which language is used to structure web pages?",
+        answer: "HTML",
+        options: [
+            "CSS",
+            "JavaScript",
+            "HTML",
+            "Python"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which of the following is a back-end programming language?",
+        answer: "Python",
+        options: [
+            "CSS",
+            "JavaScript",
+            "HTML",
+            "Python"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which of these is a version control system?",
+        answer: "Git",
+        options: [
+            "Docker",
+            "Git",
+            "Jenkins",
+            "Kubernetes"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is an array?",
+        answer: "A collection of elements",
+        options: [
+            "A collection of elements",
+            "A function parameter",
+            "A database table",
+            "A styling rule"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which of these is used to make asynchronous requests in JavaScript?",
+        answer: "AJAX",
+        options: [
+            "AJAX",
+            "JSON",
+            "SQL",
+            "PHP"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which language is primarily used for styling web pages?",
+        answer: "CSS",
+        options: [
+            "Python",
+            "CSS",
+            "JavaScript",
+            "HTML"
+        ]
+    },
+    {
+        numb: 11,
+        question: "Which of the following is a database management system?",
+        answer: "MySQL",
+        options: [
+            "React",
+            "Angular",
+            "Node.js",
+            "MySQL"
+        ]
+    },
+    {
+        numb: 12,
+        question: "What is the use of a 'for' loop in programming?",
+        answer: "To repeat a block of code a specified number of times",
+        options: [
+            "To define a variable",
+            "To create functions",
+            "To repeat a block of code a specified number of times",
+            "To style elements"
+        ]
+    },
+    {
+        numb: 13,
+        question: "Which of these is an example of a front-end framework?",
+        answer: "Angular",
+        options: [
+            "Django",
+            "Laravel",
+            "Angular",
+            "Node.js"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Simple Query Language",
+            "Standard Query Language",
+            "Structured Query Language",
+            "Sequential Query Language"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Which HTML tag is used to create a hyperlink?",
+        answer: "<a>",
+        options: [
+            "<link>",
+            "<a>",
+            "<href>",
+            "<url>"
+        ]
+    },
+    {
+        numb: 16,
+        question: "Which of these is a way to declare a variable in JavaScript?",
+        answer: "let",
+        options: [
+            "define",
+            "var",
+            "let",
+            "const"
+        ]
+    },
+    {
+        numb: 17,
+        question: "Which of the following is used to access elements in a web page using JavaScript?",
+        answer: "Document Object Model (DOM)",
+        options: [
+            "HTML",
+            "CSS",
+            "JSON",
+            "Document Object Model (DOM)"
+        ]
+    },
+    {
+        numb: 18,
+        question: "Which of the following is a Python data structure?",
+        answer: "List",
+        options: [
+            "Dictionary",
+            "List",
+            "Tuple",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 19,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheets",
+        options: [
+            "Computer Style Sheets",
+            "Cascading Style Sheets",
+            "Creative Style Sheets",
+            "Colorful Style Sheets"
+        ]
+    },
+    {
+        numb: 20,
+        question: "Which of the following is a commonly used HTTP method?",
+        answer: "GET",
+        options: [
+            "POST",
+            "GET",
+            "PUT",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 21,
+        question: "Which of these is used for server-side scripting?",
+        answer: "PHP",
+        options: [
+            "HTML",
+            "CSS",
+            "PHP",
+            "JavaScript"
+        ]
+    },
+    {
+        numb: 22,
+        question: "What is the output of '2 + 2' in Python?",
+        answer: "4",
+        options: [
+            "22",
+            "2 + 2",
+            "4",
+            "Error"
+        ]
+    },
+    {
+        numb: 23,
+        question: "What is the purpose of the 'return' statement in a function?",
+        answer: "To return a value from a function",
+        options: [
+            "To exit the loop",
+            "To stop the program",
+            "To return a value from a function",
+            "To print the output"
+        ]
+    },
+    {
+        numb: 24,
+        question: "Which of these is an example of a markup language?",
+        answer: "HTML",
+        options: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ]
+    },
+    {
+        numb: 25,
+        question: "Which of these is a Python web framework?",
+        answer: "Django",
+        options: [
+            "React",
+            "Laravel",
+            "Django",
+            "Spring"
+        ]
+    },
+    {
+        numb: 26,
+        question: "What does JSON stand for?",
+        answer: "JavaScript Object Notation",
+        options: [
+            "JavaScript Online Notation",
+            "JavaScript Object Notation",
+            "JavaScript Oriented Notation",
+            "JavaScript Open Notation"
+        ]
+    },
+    {
+        numb: 27,
+        question: "Which of these is a widely used NoSQL database?",
+        answer: "MongoDB",
+        options: [
+            "MySQL",
+            "PostgreSQL",
+            "MongoDB",
+            "SQLite"
+        ]
+    },
+    {
+        numb: 28,
+        question: "Which method is used to add an element to an array in JavaScript?",
+        answer: "push()",
+        options: [
+            "add()",
+            "append()",
+            "push()",
+            "insert()"
+        ]
+    },
+    {
+        numb: 29,
+        question: "Which of the following is a data type in JavaScript?",
+        answer: "String",
+        options: [
+            "Character",
+            "String",
+            "Integer",
+            "Boolean"
+        ]
+    },
+    {
+        numb: 30,
+        question: "Which keyword is used to define a constant in JavaScript?",
+        answer: "const",
+        options: [
+            "constant",
+            "var",
+            "const",
+            "let"
+        ]
+    },
+    {
+        numb: 31,
+        question: "Which of these is an event-driven, non-blocking I/O model in JavaScript?",
+        answer: "Node.js",
+        options: [
+            "Angular",
+            "React",
+            "Node.js",
+            "Vue.js"
+        ]
+    },
+    {
+        numb: 32,
+        question: "Which of these is an example of a boolean value?",
+        answer: "true",
+        options: [
+            "0",
+            "true",
+            "null",
+            "undefined"
+        ]
+    },
+    {
+        numb: 33,
+        question: "Which of the following is a loop structure in Python?",
+        answer: "while",
+        options: [
+            "switch",
+            "case",
+            "while",
+            "select"
+        ]
+    },
+    {
+        numb: 34,
+        question: "Which of the following is an HTTP status code for 'Not Found'?",
+        answer: "404",
+        options: [
+            "200",
+            "301",
+            "404",
+            "500"
+        ]
+    },
+    {
+        numb: 35,
+        question: "Which of these is a method to fetch data from a server in JavaScript?",
+        answer: "fetch()",
+        options: [
+            "retrieve()",
+            "getData()",
+            "fetch()",
+            "query()"
+        ]
+    },
+    {
+        numb: 36,
+        question: "What is the output of 3 * 3 in Python?",
+        answer: "9",
+        options: [
+            "6",
+            "33",
+            "9",
+            "Error"
+        ]
+    },
+    {
+        numb: 37,
+        question: "Which HTML tag is used to define an unordered list?",
+        answer: "<ul>",
+        options: [
+            "<ol>",
+            "<ul>",
+            "<li>",
+            "<list>"
+        ]
+    },
+    {
+        numb: 38,
+        question: "Which of these is a string method in JavaScript?",
+        answer: "toUpperCase()",
+        options: [
+            "toString()",
+            "toUpperCase()",
+            "isInteger()",
+            "toFixed()"
+        ]
+    },
+    {
+        numb: 39,
+        question: "Which of the following is a Python library for data analysis?",
+        answer: "Pandas",
+        options: [
+            "NumPy",
+            "TensorFlow",
+            "Pandas",
+            "Matplotlib"
+        ]
+    },
+    {
+        numb: 40,
+        question: "Which of these is a client-side storage method in web browsers?",
+        answer: "localStorage",
+        options: [
+            "MySQL",
+            "Cookies",
+            "localStorage",
+            "Session"
+        ]
+    },
+    {
+        numb: 41,
+        question: "What does API stand for?",
+        answer: "Application Programming Interface",
+        options: [
+            "Application Programming Interface",
+            "Automated Programming Interface",
+            "Application Process Interface",
+            "Application Programming Interchange"
+        ]
+    },
+    {
+        numb: 42,
+        question: "Which of the following is a valid CSS property?",
+        answer: "color",
+        options: [
+            "text",
+            "color",
+            "font",
+            "border-style"
+        ]
+    },
+    {
+        numb: 43,
+        question: "Which of these is a built-in function in Python?",
+        answer: "print()",
+        options: [
+            "print()",
+            "console.log()",
+            "echo()",
+            "display()"
+        ]
+    },
+    {
+        numb: 44,
+        question: "Which of the following is used to create responsive web design?",
+        answer: "Bootstrap",
+        options: [
+            "React",
+            "jQuery",
+            "Bootstrap",
+            "PHP"
+        ]
+    },
+    {
+        numb: 45,
+        question: "Which of these is a Python data type?",
+        answer: "Dictionary",
+        options: [
+            "Table",
+            "Dictionary",
+            "String",
+            "Set"
+        ]
+    },
+    {
+        numb: 46,
+        question: "Which SQL clause is used to filter results?",
+        answer: "WHERE",
+        options: [
+            "SELECT",
+            "WHERE",
+            "JOIN",
+            "ORDER BY"
+        ]
+    },
+    {
+        numb: 47,
+        question: "Which of the following is a JavaScript data type?",
+        answer: "Undefined",
+        options: [
+            "Undefined",
+            "Object",
+            "Array",
+            "Class"
+        ]
+    },
+    {
+        numb: 48,
+        question: "What is the correct HTML tag for inserting a line break?",
+        answer: "<br>",
+        options: [
+            "<break>",
+            "<br>",
+            "<lb>",
+            "<newline>"
+        ]
+    },
+    {
+        numb: 49,
+        question: "Which of these is a built-in module in Node.js?",
+        answer: "fs",
+        options: [
+            "express",
+            "fs",
+            "axios",
+            "react"
+        ]
+    },
+    {
+        numb: 50,
+        question: "Which of these is a string interpolation method in Python?",
+        answer: "f-strings",
+        options: [
+            "string.Template",
+            "percent (%) formatting",
+            "str.format()",
+            "f-strings"
+        ]
+    }
+    // {
+    //     numb: 51,
+    //     question: "Which of these is a way to declare a function in JavaScript?",
+    //     answer: "function",
+    //     options: [
+    //         "function",
+    //         "def",
+    //         "lambda",
+    //         "func"
+    //     ]
+    // }    
+]; 
--- script.js
+++ script.js
@@ -23,7 +23,7 @@ continueBtn.onclick = ()=>{
 }
 
 let timeValue =  15;
-let questionsCount = 0;
+let questionCount = 0;
 let questionsNumb = 1;
 let userScore = 0;
 let counter;
@@ -37,7 +37,7 @@ restart_quiz.onclick = ()=>{
     quizBox.classList.add("activeQuiz"); 
     result.classList.remove("activeResult"); 
     timeValue = 15; 
-    questionsCount = 0;
+    questionCount = 0;
     questionsNumb = 1;
     userScore = 0;
     widthValue = 0;
@@ -55,102 +55,100 @@ quit_quiz.onclick = ()=>{
     window.location.reload(); //reload the current window
 }
 
-const next_btn = document.querySelector("footer .next_btn");
-const bottom_ques_counter = document.querySelector("footer .total_que");
+const next = document.querySelector("footer .next");
+const bottom_ques_counter = document.querySelector("footer .totalQuestions");
 
 // if  the "Next Que" button is clicked
-next_btn.onclick = ()=>{
-    if(que_count < questions.length - 1){ //if question count is less than total question length
-        que_count++; //increment the que_count value
-        que_numb++; //increment the que_numb value
-        showQuetions(que_count); //calling showQestions function
-        queCounter(que_numb); //passing que_numb value to queCounter
-        clearInterval(counter); //clear counter
-        clearInterval(counterLine); //clear counterLine
-        startTimer(timeValue); //calling startTimer function
-        startTimerLine(widthValue); //calling startTimerLine function
-        timeText.textContent = "Time Left"; //change the timeText to Time Left
-        next_btn.classList.remove("show"); //hide the next button
+next.onclick = ()=>{
+    if(questionCount < quest.length - 1){ 
+        questionCount++; 
+        questionsNumb++; 
+        showQuestions(questionCount); 
+        questionsCounter(questionsNumb); 
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+        startTimer(timeValue); 
+        startTimerLine(widthValue); 
+        timeText.textContent = "Time Left"; 
+        next.classList.remove("show"); 
     }else{
-        clearInterval(counter); //clear counter
-        clearInterval(counterLine); //clear counterLine
-        showResult(); //calling showResult function
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+        showResult(); 
     }
 }
 
-// getting the questions and options from array
-function showQuetions(index){
-    const que_text = document.querySelector(".que_text");
 
-    //creating a new span and div tag for question and option and passing the value using array index
-    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
-    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
-    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
-    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+function showQuestions(index){
+    const questions = document.querySelector(".questions");
+
     
-    const option = option_list.querySelectorAll(".option");
+    let que_tag = '<span>'+ quest[index].numb + ". " + quest[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ quest[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ quest[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ quest[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ quest[index].options[3] +'</span></div>';
+    questions.innerHTML = que_tag; 
+    options.innerHTML = option_tag; 
+    
+    const option = options.querySelectorAll(".option");
 
-    // set the onclick attribute to all available options
+   
     for(i=0; i < option.length; i++){
         option[i].setAttribute("onclick", "optionSelected(this)");
     }
 }
-// creating the new div tags  for the  icons
+
 let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
 let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 //if the user clicks on the option
 function optionSelected(answer){
-    clearInterval(counter); //clear counter
-    clearInterval(counterLine); //clear counterLine
-    let userAns = answer.textContent; //getting user selected option
-    let correcAns = questions[que_count].answer; //getting correct answer from array
-    const allOptions = option_list.children.length; //getting all option items
-    
-    if(userAns == correcAns){ //if user selected option is equal to array's correct answer
-        userScore += 1; //upgrading score value with 1
-        answer.classList.add("correct"); //adding green color to correct selected option
-        answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
+    clearInterval(counter); 
+    clearInterval(counterLine); 
+    let userAns = answer.textContent; 
+    let correcAns = quest[questionCount].answer; 
+    const allOptions = options.children.length; 
+
+    if(userAns == correcAns){ 
+        userScore += 1; 
+        answer.classList.add("correct"); 
+        answer.insertAdjacentHTML("beforeend", tickIconTag); 
         console.log("Correct Answer");
         console.log("Your correct answers = " + userScore);
     }else{
-        answer.classList.add("incorrect"); //adding red color to correct selected option
-        answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
+        answer.classList.add("incorrect"); 
+        answer.insertAdjacentHTML("beforeend", crossIconTag); 
         console.log("Wrong Answer");
 
         for(i=0; i < allOptions; i++){
-            if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer 
-                option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+            if(options.children[i].textContent == correcAns){ 
+                options.children[i].setAttribute("class", "option correct"); 
+                options.children[i].insertAdjacentHTML("beforeend", tickIconTag);
                 console.log("Auto selected correct answer.");
             }
         }
     }
     for(i=0; i < allOptions; i++){
-        option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+        options.children[i].classList.add("disabled"); 
     }
-    next_btn.classList.add("show"); //show the next button if user selected any option
+    next.classList.add("show"); 
 }
-
 function showResult(){
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.remove("activeQuiz"); //hide quiz box
-    result_box.classList.add("activeResult"); //show result box
-    const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 3){ // if user scored more than 3
-        //creating a new span tag and passing the user score number and total question number
-        let scoreTag = '<span>and congrats! , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
-        scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
+    infoBox.classList.remove("activeInfo"); 
+    quizBox.classList.remove("activeQuiz"); 
+    result.classList.add("activeResult");
+    const scoreText = result.querySelector(".score");
+    if (userScore > 40){ 
+        let scoreTag = '<span>and congrats! , You got <p>'+ userScore +'</p> out of <p>'+ quest.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;  
     }
-    else if(userScore > 1){ // if user scored more than 1
-        let scoreTag = '<span>and nice , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+    else if(userScore > 20){ 
+        let scoreTag = '<span>and nice , You got <p>'+ userScore +'</p> out of <p>'+ quest.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
-    else{ // if user scored less than 1
-        let scoreTag = '<span>and sorry , You got only <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+    else{ 
+        let scoreTag = '<span>and sorry , You got only <p>'+ userScore +'</p> out of <p>'+ quest.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
 }
@@ -158,28 +156,28 @@ function showResult(){
 function startTimer(time){
     counter = setInterval(timer, 1000);
     function timer(){
-        timeCount.textContent = time; //changing the value of timeCount with time value
-        time--; //decrement the time value
-        if(time < 9){ //if timer is less than 9
+        timeCount.textContent = time; 
+        time--; 
+        if(time < 9){ 
             let addZero = timeCount.textContent; 
-            timeCount.textContent = "0" + addZero; //add a 0 before time value
+            timeCount.textContent = "0" + addZero; 
         }
-        if(time < 0){ //if timer is less than 0
-            clearInterval(counter); //clear counter
-            timeText.textContent = "Time Off"; //change the time text to time off
-            const allOptions = option_list.children.length; //getting all option items
-            let correcAns = questions[que_count].answer; //getting correct answer from array
+        if(time < 0){ 
+            clearInterval(counter); 
+            timeText.textContent = "Time Off"; 
+            const allOptions = options.children.length; 
+            let correcAns = questions[questionCount].answer; 
             for(i=0; i < allOptions; i++){
-                if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer
-                    option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+                if(options.children[i].textContent == correcAns){ 
+                    options.children[i].setAttribute("class", "option correct"); 
+                    options.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
                     console.log("Time Off: Auto selected correct answer.");
                 }
             }
             for(i=0; i < allOptions; i++){
-                option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+                options.children[i].classList.add("disabled");
             }
-            next_btn.classList.add("show"); //show the next button if user selected any option
+            next.classList.add("show"); 
         }
     }
 }
@@ -187,16 +185,15 @@ function startTimer(time){
 function startTimerLine(time){
     counterLine = setInterval(timer, 29);
     function timer(){
-        time += 1; //upgrading time value with 1
-        time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time > 549){ //if time value is greater than 549
-            clearInterval(counterLine); //clear counterLine
+        time += 1; 
+        timeline.style.width = time + "px"; 
+        if(time > 549){ 
+            clearInterval(counterLine); 
         }
     }
 }
 
-function queCounter(index){
-    //creating a new span tag and passing the question number and total question
-    let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
-    bottom_ques_counter.innerHTML = totalQueCounTag;  //adding new span tag inside bottom_ques_counter
+function questionsCounter(index){
+    let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ quest.length +'</p> Questions</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag;  
 }
